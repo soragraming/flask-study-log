deeeae47c0d5f9287727776e890ee5e454b628b9
soragraming <ohzora0817@gmail.com>
2026-02-18 00:03:53 +0900

    2026.2.17
    ・三項演算子
    ・isinstace
    ・== と is の使い分け

--- section3_基礎文法講座/base15_if.py
+++ section3_基礎文法講座/base15_if.py
@@ -1,11 +1,30 @@
 var = True
-if var: #この中の指揮がTrueの場合実行される
+
+if var: #この中の式がTrueの場合実行される
     print('varはTrueです。この中は実行されます。')
-    
+
 age = 20
 print(bool(age)) # True
-if age:
+if age: #0以外はTrue
     print(f'ageは{age}です。この中は実行されます。')
-    
-msg = ''
-if msg:
+
+msg = 'Hello'
+if msg: #msgが空白でない場合はTrueに変換
+    print(f'msg:{msg}')
+
+# 数値は0，0.0，文字列，リスト，辞書,タプル,セットは空の場合False
+
+fruits = ['apple','lemon']
+if fruits:
+    print(f'fruitsの中身は:{fruits}')
+
+age = 20
+print(age >= 20)
+
+if age >= 20: #ageが20以上の場合、True
+    print('成人です')
+
+x = 5
+if x == 5: # ==は一致するかの確認　!=は逆
+    print('xは５です')
+
